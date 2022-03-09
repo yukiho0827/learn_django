1b4e9455ff66260439e8c757ba44d5e575252c31
zrx <zrxcmk@gmail.com>
2022-03-09 19:14:51 +0800

    v4

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-def judge(year):
-    return True if not year % 400 or not year % 4 and year % 100 else False
 
-
-res = judge(2000)
-print(res)
-
-print(456)
-print(123)
-print(456)
+for i in range(5):
+    for k in range(4):
+        for index in range(3):
+            print(i, k, index)
